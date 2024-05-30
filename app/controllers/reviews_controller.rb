3af9b634626887a8c42a205cecad670631035b0c
Max Denyshchuk <denyshchukmaxim.dev@gmail.com>
2024-05-30 19:43:33 +0300

    Add Album styles

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,24 @@
 class ReviewsController < InheritedResources::Base
   def my_reviews
-    @reviews = current_user.reviews
+    @reviews = Review.where(review_by_id: current_user.id)
   end
 
   def create
-    @review = current_user.reviews.build(review_params.merge(review_by_id: current_user.id))
-    if @review.save
-      redirect_to @review, notice: 'Review was successfully created.'
-    else
-      render :new
+    @album = Album.find(params[:album_id])
+    @review = @album.reviews.build(review_params)
+    @review.user = current_user
+
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to @album, notice: 'Review was successfully created.' }
+        format.js   # This will look for create.js.erb
+      else
+        format.html { render :new }
+        format.js   { render :new }
+      end
     end
   end
+
 
   private
 
